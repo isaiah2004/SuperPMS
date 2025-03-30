cb116eddba934ba93167f430184e85ecb348505d
Isaiah <91770244+isaiah2004@users.noreply.github.com>
2025-03-30 23:39:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app.secret_key = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!/xd5\xa2\xa0\x9fR"\xa1
     don't accept 127.0.0.1 / localhost
 """
 
-app.config["SERVER_NAME"] = "localhost:5000"
+app.config["SERVER_NAME"] = "www.super-pms.onrender.com"
 oauth = OAuth(app)
 
 
